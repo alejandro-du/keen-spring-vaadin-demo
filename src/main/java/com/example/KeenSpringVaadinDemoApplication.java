eb3405ba9490ae92c0634a8de9c6ba6d5e84ca1d
alejandro-du <alejandro.d.a@gmail.com>
2016-08-03 17:12:24 +0300

    Initial commit

---
+++ src/main/java/com/example/KeenSpringVaadinDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeenSpringVaadinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeenSpringVaadinDemoApplication.class, args);
+	}
+}
